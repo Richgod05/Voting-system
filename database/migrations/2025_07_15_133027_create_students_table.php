357efe015f887d1367a7ec08c8b9d0dc5f2f1976
Richgod05 <richgodpallangyo@gmail.com>
2025-07-22 21:46:43 +0300

    changes

--- database/migrations/2025_07_15_133027_create_students_table.php
+++ database/migrations/2025_07_15_133027_create_students_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('email');
             $table->string('password');
+            $table->enum('role', ['admin', 'student'])->default('student');
             $table->timestamps();
         });
     }
